039aeebfe2fa9f7212f5619d6fd64e5463e66f2a
anna.tiala <anna.tiala@siili.com>
2019-10-22 15:02:58 +0300

    style updates

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import CvBlock from "../CvBlock/CvBlock";
+import PortfolioBlock from "../PortfolioBlock/PortfolioBlock";
 import "./Main.scss";
 
 export default function Main() {
     return (
         <div className="main-view">
             <CvBlock />
+            <PortfolioBlock />
         </div>
     )
 }
---
+++ src/components/PortfolioBlock/PortfolioBlock.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import PortfolioImage from "../../images/art.png";
+import "./PortfolioBlock.scss";
+
+export default function PortfolioBlock() {
+    return (
+        <div className="portfolio-block">
+            <div className="container">
+                <div className="flex-row">
+                    <div className="flex-left">
+                        <h1 className="cv-title">Portfolioni</h1>
+                        <h3>Käy selaamassa töitäni portfoliostani. Portfolioon on listattu mm. tekemiäni nettisivuprojekteja, web-designia, graafista designia, ottamiani valokuvia ja tekemiäni piirustuksia ja maalauksia.</h3>
+                    </div>
+                    <div className="flex-right">
+                        <div className="image-area">
+                            <img className="portfolio-image" src={PortfolioImage} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
