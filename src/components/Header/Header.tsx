f15b2ee93086b99f5a392ca0705913eefed1ce37
anna.tiala <anna.tiala@siili.com>
2019-10-28 14:03:22 +0200

    header style changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,18 +6,14 @@ export default function Header() {
     return (
         <div className="header">
             <div className="container">
-                <div className="flex-row">
-                    <div className="flex-left">
-                        <h1 className="header-title">Portfolio</h1>
-                    </div>
-                    <div className="flex-right">
-                        <div className="flex-row">
-                            <div className="image-cropper">
-                                <img className="header-image" src={HeaderImage} alt="" />
-                            </div>
-                            <h1 className="header-title-name">Anna Tiala</h1>
+                <div className="header-text-area">
+                    <div className="flex-row">
+                        <div className="image-cropper">
+                            <img className="header-image" src={HeaderImage} alt="" />
                         </div>
+                        <h1 className="header-title-name">Anna Tiala</h1>
                     </div>
+                    <h1 className="header-title">Portfolio</h1>
                 </div>
             </div>
         </div>
