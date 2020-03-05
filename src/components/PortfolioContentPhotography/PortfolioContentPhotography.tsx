d3ac92c0c1f9e00c7928cdfc082994b62ad8d92f
anna.tiala <anna.tiala@siili.com>
2020-03-05 15:02:01 +0200

    adding new images

--- src/components/PortfolioContentPhotography/PortfolioContentPhotography.tsx
+++ src/components/PortfolioContentPhotography/PortfolioContentPhotography.tsx
@@ -1,17 +1,23 @@
 import * as React from "react";
 import PregnancyImage from "../../images/pregnancy.jpg";
-import BirdsImage from "../../images/agility.jpg";
 import AgilityImage from "../../images/agility.jpg";
 import ManImage from "../../images/dad.jpg";
 import CatImage from "../../images/cat.jpg";
+import KokkolaImage from "../../images/kokkola.jpg";
+import TwoDogsImage from "../../images/twodogs.jpg";
+import SicilyImage from "../../images/sisilia.jpeg";
 import "./PortfolioContentPhotography.scss";
 
 export default function PortfolioContentPhotography() {
     return (
         <div className="show-content-photos">
+            <div className="row">  
+                <div className="imagebox"><img src={TwoDogsImage} alt="Two dogs" /></div>
+                <div className="imagebox"><img src={SicilyImage} alt="Man in Sicily" /></div>
+            </div>
             <div className="row">
                 <div className="imagebox"><img src={PregnancyImage} alt="Pregnant woman" /></div>
-                <div className="imagebox"><img src={BirdsImage} alt="Birds on the sky" /></div>
+                <div className="imagebox"><img src={KokkolaImage} alt="Birds in Kokkola" /></div>
                 <div className="imagebox"><img src={AgilityImage} alt="Dog and agility" /></div>
             </div>
             <div className="row">
