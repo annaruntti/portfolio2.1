c5149af67ca22028ffb76ed8cea66979d331a1cf
anna.tiala <anna.tiala@siili.com>
2020-03-17 18:58:13 +0200

    adding graphic designs to portfolio page

--- src/components/PortfolioContentDesign/PortfolioContentDesign.tsx
+++ src/components/PortfolioContentDesign/PortfolioContentDesign.tsx
@@ -6,6 +6,9 @@ import PuppyFrontPage from "../../images/puppy-frontpage.png";
 import LitterDataImage from "../../images/puppy-litter-data.png";
 import AddPuppysImage from "../../images/puppy-add-litter-puppys.png";
 import MarkWeightImage from "../../images/puppy-litter-data-mark-weight.png";
+import LitterPlansImage from "../../images/umajarymy.jpg"
+import LitterPlansImage2 from "../../images/mainos.jpg"
+import LitterPlansImage3 from "../../images/mainos2.jpg"
 import "./PortfolioContentDesign.scss";
 
 export default function PortfolioContentDesign() {
@@ -44,6 +47,29 @@ export default function PortfolioContentDesign() {
                         </div>
                         <div className="image-wrapper">
                             <img src={MarkWeightImage} alt="Web site layout" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="project-box">
+                <div className="row">
+                    <div className="text-introduction">
+                        <h4>Pentuemainokset</h4>
+                        <p>
+                            Olen tehnyt oman kennelini pentuesuunnitelmista, sekä muiden tuttujen kasvattajien pentuesuunnitelmista mainoksia, joissa on ollut tietoa ja kuvia tulevan pentueen vanhemmista. Tässä joitakin tekemistäni mainoksista. Mainokset olen tehnyt käyttäen Adobe Photoshop -ohjelmaa.
+                        </p>
+                    </div>
+                </div>
+                <div className="image-box">
+                    <div className="row">
+                        <div className="image-wrapper">
+                            <img className="litterplan-image" src={LitterPlansImage} alt="Web site layout" />
+                        </div>
+                        <div className="image-wrapper">
+                            <img className="litterplan-image" src={LitterPlansImage3} alt="Web site layout" />
+                        </div>
+                        <div className="image-wrapper">
+                            <img className="litterplan-image" src={LitterPlansImage2} alt="Web site layout" />
                         </div>
                     </div>
                 </div>
