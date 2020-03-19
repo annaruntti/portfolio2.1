66af49bf46d7e78c8163cf296fd4f73490f950ff
anna.tiala <anna.tiala@siili.com>
2020-03-19 18:48:36 +0200

    style fixes

--- src/components/ContactView/ContactView.tsx
+++ src/components/ContactView/ContactView.tsx
@@ -7,44 +7,55 @@ import ProfileImage from "../../images/profile.jpg";
 import "./ContactView.scss";
 
 export default function ContactView() {
-    return (
-        <div className="contact-view">
-            <ContactTitleBlock />
-            <div className="contact-area">
-                <div className="container">
-                    <div className="flex-row">
-                        <div className="flex-item-left">
-                            <div className="info-details">
-                                <div className="image-cropper">
-                                    <img className="header-image" src={ProfileImage} alt="My profile image" />
-                                </div>
-                                <div className="contat-info">
-                                    <h3>Yhteystiedot</h3>
-                                    <p>linkedin.com/in/annaruntti/<br />
-                                        suomenlapinkoira.net<br />
-                                        anruntti@gmail.com<br />
-                                        +358452346943<br />
-                                        Haravatie 27<br />
-                                        90530 Oulu<br />
-                                        s. 6.4.1990</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex-item-right">
-                            <div className="contact-form-area">
-                                <div className="alert alert-info" role="alert">
-                                    <h3>Ota yhteyttä!</h3>
-                                    <p>Voit ottaa minuun yhteyttä oheisella formilla.</p>
-                                    <p>Huom. päivitys 17.3.2020, yhteydenottolomake ei toimi juuri nyt, otathan yhteyttä sähköpostitse.</p>
-                                </div>
-                                <ContactForm />
-                                {/* <CvLinkBlock /> */}
-                                {/* <PortfolioLinkBlock /> */}
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div className="contact-view">
+      <ContactTitleBlock />
+      <div className="contact-area">
+        <div className="container">
+          <div className="flex-row">
+            <div className="flex-item-left">
+              <div className="info-details">
+                <div className="image-cropper">
+                  <img className="header-image" src={ProfileImage} alt="Me" />
                 </div>
+                <div className="contat-info">
+                  <h3>Yhteystiedot</h3>
+                  <p>
+                    linkedin.com/in/annaruntti/
+                    <br />
+                    suomenlapinkoira.net
+                    <br />
+                    anruntti@gmail.com
+                    <br />
+                    +358452346943
+                    <br />
+                    Haravatie 27
+                    <br />
+                    90530 Oulu
+                    <br />
+                    s. 6.4.1990
+                  </p>
+                </div>
+              </div>
             </div>
+            <div className="flex-item-right">
+              <div className="contact-form-area">
+                <div className="alert alert-info" role="alert">
+                  <h3>Ota yhteyttä!</h3>
+                  <p>Voit ottaa minuun yhteyttä oheisella formilla.</p>
+                  <p>
+                    Huom. päivitys 17.3.2020, yhteydenottolomake ei toimi juuri
+                    nyt, otathan yhteyttä sähköpostitse.
+                  </p>
+                </div>
+                <ContactForm />
+                {/* <CvLinkBlock /> */}
+                {/* <PortfolioLinkBlock /> */}
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
