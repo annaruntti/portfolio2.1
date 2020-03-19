0773f82b90fd56d84f73012c791be8dd71bf82fa
anna.tiala <anna.tiala@siili.com>
2020-03-19 15:55:35 +0200

    style fixes

--- src/components/PortfolioArea/PortfolioArea.tsx
+++ src/components/PortfolioArea/PortfolioArea.tsx
@@ -1,68 +1,72 @@
-import React, { Component, MouseEvent } from "react";
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { makeStyles } from "@material-ui/core/styles";
 
-import PortfolioContentPhotography from "../PortfolioContentPhotography/PortfolioContentPhotography"
-import PortfolioContentDesign from "../PortfolioContentDesign/PortfolioContentDesign"
+import PortfolioContentPhotography from "../PortfolioContentPhotography/PortfolioContentPhotography";
+import PortfolioContentDesign from "../PortfolioContentDesign/PortfolioContentDesign";
 import "./PortfolioArea.scss";
 
-interface Props {
-    class?: string;
-}
-
 const useStyles = makeStyles({
-    showContent: {
-        display: 'block',
-    },
-    hideContent: {
-        display: 'none',
-    }
+  showContent: {
+    display: "block"
+  },
+  hideContent: {
+    display: "none"
+  }
 });
 
 interface BlockProps {
-    title: string;
-    children: React.ReactNode;
-    open?: Boolean
-};
+  title: string;
+  children: React.ReactNode;
+  open?: Boolean;
+}
 
-const PortfolioAreaBlock: React.FC<BlockProps> = ({title, children, open = false}) => {
-    const classes = useStyles({});
-    const [isOpen, setIsOpen] = React.useState(open);
+const PortfolioAreaBlock: React.FC<BlockProps> = ({
+  title,
+  children,
+  open = false
+}) => {
+  const classes = useStyles({});
+  const [isOpen, setIsOpen] = React.useState(open);
 
-    return (
-        <>
-        <div className="flex-title-row">
+  return (
+    <>
+      <div className="flex-title-row">
         <h3>{title}</h3>
         <div className="icon-item">
-            <button className="show-content-btn" onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? (<ExpandLessIcon />):(<ExpandMoreIcon />)}
-            </button>
+          <button
+            className="show-content-btn"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          </button>
         </div>
-    </div>
-    <div className={isOpen ? classes.showContent : classes.hideContent}>
+      </div>
+      <div className={isOpen ? classes.showContent : classes.hideContent}>
         {children}
-    </div>
-</>
-    );
+      </div>
+    </>
+  );
+};
+
+interface Props {
+  class?: string;
 }
 
 const PortfolioArea: React.FC<Props> = () => {
-    const classes = useStyles({});
-
-    return (
-
-        <div className="portfolio-area">
-            <div className="container">
-            <PortfolioAreaBlock title="Valokuvaus" open={true}>
-                <PortfolioContentPhotography/>
-            </PortfolioAreaBlock>
-            <PortfolioAreaBlock title="Design ja grafiikka">
-                <PortfolioContentDesign/>
-            </PortfolioAreaBlock>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="portfolio-area">
+      <div className="container">
+        <PortfolioAreaBlock title="Valokuvaus" open={true}>
+          <PortfolioContentPhotography />
+        </PortfolioAreaBlock>
+        <PortfolioAreaBlock title="Design ja grafiikka">
+          <PortfolioContentDesign />
+        </PortfolioAreaBlock>
+      </div>
+    </div>
+  );
+};
 
 export default PortfolioArea;
