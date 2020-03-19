75707b3ee8314e160e295b47ee341bc88289ee94
anna.tiala <anna.tiala@siili.com>
2020-03-19 15:55:23 +0200

    adding for buttons showing and hiding functions

--- src/components/PortfolioArea/PortfolioArea.tsx
+++ src/components/PortfolioArea/PortfolioArea.tsx
@@ -1,48 +1,68 @@
 import React, { Component, MouseEvent } from "react";
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
 
 import PortfolioContentPhotography from "../PortfolioContentPhotography/PortfolioContentPhotography"
 import PortfolioContentDesign from "../PortfolioContentDesign/PortfolioContentDesign"
 import "./PortfolioArea.scss";
 
-export default class PortfolioArea extends React.Component {
-    handleClick(e: MouseEvent<HTMLButtonElement>) {
-        e.preventDefault();
-        alert(e.currentTarget.tagName); // alerts BUTTON
-    }
-
-    handleAnotherClick(e: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) {
-        e.preventDefault();
-        alert('Yeah!');
-    }
-
-    render() {
-        return (
-            <div className="portfolio-area">
-                <div className="container">
-                    <div className="flex-title-row">
-                        <h3>Valokuvaus</h3>
-                        <div className="icon-item">
-                            <button
-                                onClick={this.handleClick}
-                                className="show-content-btn">{this.props.children}<ExpandLessIcon />
-                            </button>
-                        </div>
-                    </div>
-                    <PortfolioContentPhotography/>
-                    <div className="flex-title-row">
-                        <h3>Design ja grafiikka</h3>
-                        <div className="icon-item">
-                            <button
-                                onClick={this.handleClick}
-                                className="show-content-btn">{this.props.children}<ExpandLessIcon />
-                            </button>
-                        </div>
-                    </div>
-                    <PortfolioContentDesign/>
-                </div>
-            </div>
-        );
-    }
+interface Props {
+    class?: string;
 }
+
+const useStyles = makeStyles({
+    showContent: {
+        display: 'block',
+    },
+    hideContent: {
+        display: 'none',
+    }
+});
+
+interface BlockProps {
+    title: string;
+    children: React.ReactNode;
+    open?: Boolean
+};
+
+const PortfolioAreaBlock: React.FC<BlockProps> = ({title, children, open = false}) => {
+    const classes = useStyles({});
+    const [isOpen, setIsOpen] = React.useState(open);
+
+    return (
+        <>
+        <div className="flex-title-row">
+        <h3>{title}</h3>
+        <div className="icon-item">
+            <button className="show-content-btn" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? (<ExpandLessIcon />):(<ExpandMoreIcon />)}
+            </button>
+        </div>
+    </div>
+    <div className={isOpen ? classes.showContent : classes.hideContent}>
+        {children}
+    </div>
+</>
+    );
+}
+
+const PortfolioArea: React.FC<Props> = () => {
+    const classes = useStyles({});
+
+    return (
+
+        <div className="portfolio-area">
+            <div className="container">
+            <PortfolioAreaBlock title="Valokuvaus" open={true}>
+                <PortfolioContentPhotography/>
+            </PortfolioAreaBlock>
+            <PortfolioAreaBlock title="Design ja grafiikka">
+                <PortfolioContentDesign/>
+            </PortfolioAreaBlock>
+            </div>
+        </div>
+    );
+}
+
+export default PortfolioArea;
