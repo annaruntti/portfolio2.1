b0cb9a67a8105dc25ca98282942be78ba0327a44
anna.tiala <anna.tiala@siili.com>
2020-05-20 10:52:00 +0300

    form label fix

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -5,38 +5,32 @@ export default function ContactForm() {
   return (
     // <form ref="postForm" onSubmit={this.onFormSubmit}>
     <form className="contact-form">
-      <label>
-        Nimi
-        <input
-          id="name"
-          name="name"
-          // ref="name"
-          type="text"
-          // value={this.state.nimi}
-          // onChange={this.handleNameChange}
-        />
-      </label>
-      <label>
-        Sähköpostiosoite
-        <input
-          id="email"
-          name="email"
-          // ref="email"
-          type="email"
-          // value={this.state.nimi}
-          // onChange={this.handleNameChange}
-        />
-      </label>
-      <label>
-        Viestisi
-        <textarea
-          id="message"
-          name="message"
-          // ref="message"
-          // value={this.state.nimi}
-          // onChange={this.handleNameChange}
-        />
-      </label>
+      <label>Nimi</label>
+      <input
+        id="name"
+        name="name"
+        // ref="name"
+        type="text"
+        // value={this.state.nimi}
+        // onChange={this.handleNameChange}
+      />
+      <label>Sähköpostiosoite</label>
+      <input
+        id="email"
+        name="email"
+        // ref="email"
+        type="email"
+        // value={this.state.nimi}
+        // onChange={this.handleNameChange}
+      />
+      <label>Viestisi</label>
+      <textarea
+        id="message"
+        name="message"
+        // ref="message"
+        // value={this.state.nimi}
+        // onChange={this.handleNameChange}
+      />
       <div className="btn-area">
         <button className="submit-btn" type="submit" value="Submit">
           Lähetä
