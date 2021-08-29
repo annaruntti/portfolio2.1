d94a9dbf259f699cd9624066588b38fa30199793
anna.tiala <anna.tiala@siili.com>
2021-08-29 14:22:44 +0300

    adding emailjs package

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,38 +1,41 @@
 import * as React from "react";
+import emailjs from "emailjs-com";
 import "./ContactForm.scss";
 
 export default function ContactForm() {
+  function sendEmail(e: React.FormEvent<HTMLFormElement>) {
+    console.log("löhetys");
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_hhewjpa",
+        "template_zky08yf",
+        e.currentTarget,
+        "user_ro1VClz7wFbFRzSP9oXsv"
+      )
+      .then(
+        result => {
+          console.log(result.text);
+        },
+        error => {
+          console.log(error.text);
+        }
+      );
+  }
+
   return (
-    // <form ref="postForm" onSubmit={this.onFormSubmit}>
-    <form className="contact-form">
+    <form className="contact-form" onSubmit={sendEmail}>
       <label>Nimi</label>
-      <input
-        id="name"
-        name="name"
-        // ref="name"
-        type="text"
-        // value={this.state.nimi}
-        // onChange={this.handleNameChange}
-      />
+      <input id="name" name="name" type="text" />
       <label>Sähköpostiosoite</label>
-      <input
-        id="email"
-        name="email"
-        // ref="email"
-        type="email"
-        // value={this.state.nimi}
-        // onChange={this.handleNameChange}
-      />
+      <input id="email" name="email" type="email" />
+      <label>Viestin aihe</label>
+      <input id="subject" name="subject" type="text" />
       <label>Viestisi</label>
-      <textarea
-        id="message"
-        name="message"
-        // ref="message"
-        // value={this.state.nimi}
-        // onChange={this.handleNameChange}
-      />
+      <textarea id="message" name="message" />
       <div className="btn-area">
-        <button className="submit-btn" type="submit" value="Submit">
+        <button className="submit-btn" type="submit" value="Send message">
           Lähetä
         </button>
       </div>
